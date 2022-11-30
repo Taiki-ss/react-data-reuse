272629b0d123f24a43219b5efd48fb95777f0c97
Taiki-nk <taikifate.0206@gmail.com>
2022-11-30 23:40:57 +0900

    :+1:[feat] データ取得成功

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Navi from "../components/Navi";
+import usersData from "../data/users";
 
 const Users: React.FC = () => {
+  const data = usersData();
+  console.log(data);
   return (
     <div>
       <Navi />
